12dd2c272d25cabd8c5c96e02aa9a9877d86d7e3
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-09-26 20:43:24 -0300

    Create Publicacao.ts

---
+++ src/models/Publicacao.ts
@@ -0,0 +1,16 @@
+import { ItemAcervo } from './ItemAcervo';
+
+export abstract class Publicacao extends ItemAcervo {
+    constructor(
+        titulo: string,
+        ano: number,
+        public autor: string,
+        public editora: string
+    ) {
+        super(titulo, ano);
+    }
+
+    descricao(): string {
+        return `${this.titulo}, por ${this.autor}, publicado por ${this.editora} em ${this.ano}.`;
+    }
+}
