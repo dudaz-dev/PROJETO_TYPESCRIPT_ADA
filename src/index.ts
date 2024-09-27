3d5f7fa38835f1d1d9b53b6fc1faf3a8c694c8cb
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-09-27 19:37:17 -0300

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,9 @@
 import * as readline from 'readline-sync';
 import { BibliotecaService } from './services/BibliotecaService';
-import { Livro } from './models/Livro';
-import { CD } from './models/CD';
-import { DVD } from './models/DVD';
+import { Livro } from './models/library/Livro';
+import { CD } from './models/library/CD';
+import { DVD } from './models/library/DVD';
+import { Revista } from './models/library/Revista';
 
 const biblioteca = new BibliotecaService();
 
@@ -11,9 +12,10 @@ function menu() {
         1 - Adicionar Livro
         2 - Adicionar CD
         3 - Adicionar DVD
-        4 - Remover Item
-        5 - Listar Acervo
-        6 - Atualizar Disponibilidade
+        4 - Adicionar Revista
+        5 - Remover Item
+        6 - Listar Acervo
+        7 - Atualizar Disponibilidade
         0 - Sair
     `)
 }
@@ -24,43 +26,53 @@ do {
     menu();
     opcao = readline.questionInt('Escolha uma opção: ');
 
-    switch(opcao) {
+    switch (opcao) {
         case 1:
             const tituloLivro = readline.question('Título do Livro: ');
             const anoLivro = readline.questionInt('Ano de Publicação: ');
             const autor = readline.question('Autor: ');
             const editora = readline.question('Editora: ');
-            const localizacao = readline.question('Localização: ');
-
-            const livro = new Livro(tituloLivro, anoLivro, autor, editora, localizacao);
+            const localizacaoLivro = readline.question('Localização do Livro: ');
+            const isbn = readline.question('ISBN: ');
+            const livro = new Livro(tituloLivro, anoLivro, autor, editora, localizacaoLivro, isbn);
             biblioteca.adicionarItem(livro);
             break;
         case 2:
             const tituloCD = readline.question('Título do CD: ');
-            const anoCD = readline.questionInt('Ano de Lançamento: ');
-            const duracaoCD = readline.questionInt('Duração (minutos): ');
+            const anoCD = readline.questionInt('Ano de Publicação: ');
             const artista = readline.question('Artista: ');
-
-            const cd = new CD(tituloCD, anoCD, duracaoCD, artista);
+            const duracaoCD = readline.questionInt('Duração (minutos): ');
+            const localizacaoCD = readline.question('Localização do CD: ');
+            const cd = new CD(tituloCD, anoCD, duracaoCD, artista, localizacaoCD);
             biblioteca.adicionarItem(cd);
             break;
         case 3:
             const tituloDVD = readline.question('Título do DVD: ');
-            const anoDVD = readline.questionInt('Ano de Lançamento: ');
-            const duracaoDVD = readline.questionInt('Duração (minutos): ');
+            const anoDVD = readline.questionInt('Ano de Publicação: ');
             const diretor = readline.question('Diretor: ');
-
-            const dvd = new DVD(tituloDVD, anoDVD, duracaoDVD, diretor);
+            const duracaoDVD = readline.questionInt('Duração (minutos): ');
+            const localizacaoDVD = readline.question('Localização do DVD: ');
+            const dvd = new DVD(tituloDVD, anoDVD, duracaoDVD, diretor, localizacaoDVD);
             biblioteca.adicionarItem(dvd);
             break;
         case 4:
+            const tituloRevista = readline.question('Título da Revista: ');
+            const anoRevista = readline.questionInt('Ano de Publicação: ');
+            const autorRevista = readline.question('Autor: ');
+            const editoraRevista = readline.question('Editora: ');
+            const edicaoRevista = readline.questionInt('Edição da Revista: ');
+            const localizacaoRevista = readline.question('Localização da Revista: ');
+            const revista = new Revista(tituloRevista, anoRevista, autorRevista, editoraRevista, edicaoRevista, localizacaoRevista);
+            biblioteca.adicionarItem(revista);
+            break;
+        case 5:
             const idRemover = readline.questionInt('ID do Item a ser removido: ');
             biblioteca.removerItem(idRemover);
             break;
-        case 5:
+        case 6:
             biblioteca.listarAcervo();
             break;
-        case 6:
+        case 7:
             const idAtualizar = readline.questionInt('ID do Item: ');
             let disponivelInput: string;
             do {
@@ -69,7 +81,7 @@ do {
                     console.log('Entrada inválida. Digite "sim" ou "nao".');
                 }
             } while (disponivelInput !== 'sim' && disponivelInput !== 'nao');
-            
+
             const disponivel = disponivelInput === 'sim';
             biblioteca.atualizarDisponibilidade(idAtualizar, disponivel);
             break;
