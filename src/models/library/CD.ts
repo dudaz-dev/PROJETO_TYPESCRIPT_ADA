26c82da1cb4247de1d2a733834a31e0237adc585
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-09-27 19:37:34 -0300

    Create CD.ts

---
+++ src/models/library/CD.ts
@@ -0,0 +1,18 @@
+import { Midia } from './Midia';
+import { Localizavel } from './Localizavel';
+
+export class CD extends Midia implements Localizavel{
+    constructor(
+        titulo: string,
+        ano: number,
+        duracao: number,
+        public artista: string,
+        public localizacao: string
+    ) {
+        super(titulo, ano, duracao);
+    }
+
+    descricao(): string {
+        return `CD: ${super.descricao()} Artista: ${this.artista} Localização: ${this.localizacao}`;
+    }
+}
