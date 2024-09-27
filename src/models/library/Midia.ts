7e3460dbf383693993adc32195c018982dbbf92b
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-09-27 19:37:54 -0300

    Create Midia.ts

---
+++ src/models/library/Midia.ts
@@ -0,0 +1,15 @@
+import { ItemAcervo } from './ItemAcervo';
+
+export abstract class Midia extends ItemAcervo {
+    constructor(
+        titulo: string,
+        ano: number,
+        public duracao: number
+    ) {
+        super(titulo, ano);
+    }
+
+    descricao(): string {
+        return `${this.titulo}, duração de ${this.duracao} minutos, lançado em ${this.ano}.`;
+    }
+}
