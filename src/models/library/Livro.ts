770034f9be8b9961a124f485f75c5586e56c79fb
dudaz-dev <dudaz.dev@gmail.com>
2024-09-27 21:12:00 -0300

    feat: adicionado método editar

--- src/models/library/Livro.ts
+++ src/models/library/Livro.ts
@@ -1,5 +1,7 @@
 import { Publicacao } from "./Publicacao";
 import { Localizavel } from "./Localizavel";
+import * as readline from "readline-sync";
+import { questionInt } from "../../utils/questionInt";
 
 export class Livro extends Publicacao implements Localizavel {
   public isbn: string;
@@ -17,12 +19,29 @@ export class Livro extends Publicacao implements Localizavel {
   }
 
   descricao(): string {
-    return `${super.descricao()} | Localização: ${this.localizacao} | ISBN: ${
-      this.isbn
-    }`;
+    return `${super.descricao()} | Localização: ${this.localizacao} | ISBN: ${this.isbn
+      }`;
   }
 
-  editar(): void {
-    throw new Error("Method not implemented.");
+  editar() {
+    // Pergunta por todas as propriedades do Livro
+    const titulo = readline.question(`Título do Livro [${this.titulo}]: `);
+    const ano = questionInt(`Ano de Publicação [${this.ano}]: `);
+    const editora = readline.question(`Editora [${this.editora}]: `);
+    const autor = readline.question(`Autor [${this.autor}]: `);
+    const isbn = readline.question(`ISBN [${this.isbn}]: `);
+    const localizacao = readline.question(
+      `Localização do Livro [${this.localizacao}]: `
+    );
+
+    // Atualiza as propriedades do Livro com as novas informações, se houver
+    this.titulo = titulo || this.titulo;
+    this.ano = ano || this.ano;
+    this.editora = editora || this.editora;
+    this.autor = autor || this.autor;
+    this.isbn = isbn || this.isbn;
+    this.localizacao = localizacao || this.localizacao;
+
+    console.log(`Livro "${this.titulo}" foi editado.`);
   }
 }
--- src/models/library/Revista.ts
+++ src/models/library/Revista.ts
@@ -1,5 +1,7 @@
 import { Publicacao } from "./Publicacao";
 import { Localizavel } from "./Localizavel";
+import * as readline from "readline-sync";
+import { questionInt } from "../../utils/questionInt";
 
 export class Revista extends Publicacao implements Localizavel {
   constructor(
@@ -14,12 +16,29 @@ export class Revista extends Publicacao implements Localizavel {
   }
 
   descricao(): string {
-    return `Revista: ${super.descricao()} Edição: ${this.edicao} Localização: ${
-      this.localizacao
-    }`;
+    return `Revista: ${super.descricao()} Edição: ${this.edicao} Localização: ${this.localizacao
+      }`;
   }
 
-  editar(): void {
-    throw new Error("Method not implemented.");
+  editar() {
+    // Pergunta por todas as propriedades da Revista
+    const titulo = readline.question(`Título da Revista [${this.titulo}]: `);
+    const ano = questionInt(`Ano de Publicação [${this.ano}]: `);
+    const editora = readline.question(`Editora [${this.editora}]: `);
+    const autor = readline.question(`Autor [${this.autor}]: `);
+    const edicao = questionInt(`Edição [${this.edicao}]: `);
+    const localizacao = readline.question(
+      `Localização da Revista [${this.localizacao}]: `
+    );
+
+    // Atualiza as propriedades da Revista com as novas informações, se houver
+    this.titulo = titulo || this.titulo;
+    this.ano = ano || this.ano;
+    this.editora = editora || this.editora;
+    this.autor = autor || this.autor;
+    this.edicao = edicao || this.edicao;
+    this.localizacao = localizacao || this.localizacao;
+
+    console.log(`Revista "${this.titulo}" foi editada.`);
   }
 }
